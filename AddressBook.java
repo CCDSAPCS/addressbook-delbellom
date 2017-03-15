0b1f5f1aa2af0b62b876cc72296ab26b0ef4dc22
delbellom <delbellom@countryday.net>
2017-03-15 09:08:34 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -11,3 +11,24 @@ public class AddressBook {
 		friends = new Contact[10];
 		numfriends = 0;
     }
+
+public void addContact(Contact c){
+	friends[numfriends] = c;
+	numFriends++;
+}
+
+public void printContacts(){
+	for (int i=0; i<numfriends;i++)
+		friends[i].printContact();
+}
+//	for (contact cc: AddressBook)
+//		friends[cc].printContact();
+// }
+
+public void int numContacts(){
+	return numfriends;
+}
+	
+public void haveContact(String s){
+	//interate through list of friends i=0 to numfriends
+	if (friends[i].getname()
